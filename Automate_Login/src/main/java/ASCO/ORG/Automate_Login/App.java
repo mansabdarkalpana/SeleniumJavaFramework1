68ecc3748c13ae1efc1aa8768a6f5caeaa15b97b
Kalpana.Mansabdar <Kalpana.Mansabdar@PXD10-0016.asco.local>
2020-08-20 16:27:24 -0400

    First commit on 08/20/2020 4:27PM

---
+++ Automate_Login/src/main/java/ASCO/ORG/Automate_Login/App.java
@@ -0,0 +1,13 @@
+package ASCO.ORG.Automate_Login;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
